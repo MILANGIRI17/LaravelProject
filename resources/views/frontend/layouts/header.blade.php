65ced7555d45087ad1c064ee6419288cffeeebcd
Milan Giri <milan.giri17021999@gmail.com>
2021-03-02 09:04:29 +0545

    E-Shopper template implemented

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,12 +1,28 @@
 @section('header')
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$title}}</title>
-</head>
-<body>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>{{$title}}</title>
+        <link href="{{url('frontend/css/bootstrap.min.css')}}" rel="stylesheet">
+        <link href="{{url('frontend/css/font-awesome.min.css')}}" rel="stylesheet">
+        <link href="{{url('frontend/css/prettyPhoto.css')}}" rel="stylesheet">
+        <link href="{{url('frontend/css/price-range.css')}}" rel="stylesheet">
+        <link href="{{url('frontend/css/animate.css')}}" rel="stylesheet">
+        <link href="{{url('frontend/css/main.css')}}" rel="stylesheet">
+        <link href="{{url('frontend/css/responsive.css')}}" rel="stylesheet">
+    <!--[if lt IE 9]>
+        <script src="{{url('frontend/js/html5shiv.js')}}"></script>
+        <script src="{{url('frontend/js/respond.min.js')}}"></script>
+        <![endif]-->
+        <link rel="{{url('shortcut icon" href="images/ico/favicon.ico')}}">
+        <link rel="{{url('apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png')}}">
+        <link rel="{{url('apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png')}}">
+        <link rel="{{url('apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png')}}">
+        <link rel="{{url('apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png')}}">
+    </head><!--/head-->
+
+    <body>
 @endsection
