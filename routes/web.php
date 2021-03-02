199d72fdfca60ba06c190ed9b042601ecc9d406f
Milan Giri <milan.giri17021999@gmail.com>
2021-03-02 09:19:22 +0545

    backend part defined in route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+//Frontend routes
 Route::group(['namespace'=>'frontend'],function(){
    Route::any('/','ApplicationController@index')->name('index');
    Route::any('contact','ApplicationController@contact')->name('contact');
+});
+
+
+//Backend routes
+Route::group(['namespace'=>'backend','prefix'=>'admin'],function(){
+    Route::any('/','DashboardController@index')->name('admin');
 });
