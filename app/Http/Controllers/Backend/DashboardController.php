904b0885be40ddf9a705f751eae75e9f3b44dc5f
Milan Giri <milan.giri17021999@gmail.com>
2021-03-03 08:54:45 +0545

    Backend template updated

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class DashboardController extends BackendController
 {
-
+    public function index(){
+        return view($this->pagePath.'dashboard.dashboard',$this->data);
+    }
 }
