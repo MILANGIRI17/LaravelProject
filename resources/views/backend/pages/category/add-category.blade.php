a6315d35efb2f8b8a72b0eeff8cc824ee35348b1
Milan Giri <milan.giri17021999@gmail.com>
2021-03-16 13:29:34 +0545

    add page updated to go to  show dynamic

--- resources/views/backend/pages/category/add-category.blade.php
+++ resources/views/backend/pages/category/add-category.blade.php
@@ -8,7 +8,9 @@
                 <div class="col-md-12">
                     <div class="x_panel">
                         <div class="x_title">
-                            <h2>Add Category</h2>
+                            <h2>Add Category
+                                <a href="{{route('category')}}" class="btn-sm btn-success"><i class="fa fa-eye"></i></a>
+                            </h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                 </li>
